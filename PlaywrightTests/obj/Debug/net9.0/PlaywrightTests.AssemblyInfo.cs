c7612352685cbd3f19490061ef7bfca21cabbf6d
Tibor Szekeres <tiboralex99@gmail.com>
2025-06-11 14:17:15 +0200

    add new README.md file

--- PlaywrightTests/obj/Debug/net9.0/PlaywrightTests.AssemblyInfo.cs
+++ PlaywrightTests/obj/Debug/net9.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e578920c873d5120b218ef182d5942c27fdf9757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6cb755865c2cd0484cf212583f165f764476f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,8 @@
+# README file for this project.
+
+I've used this command to run the tests from terminal: 
+
+"dotnet test -- Playwright.BrowserName=chromium Playwright.LaunchOptions.Headless=false Playwright.LaunchOptions.Channel=msedge"
+
+This test is flaky: CheckSelectJurisdictionTestByText. I would be happy to talk about the solution in a meeting I just did not want to spend more time than needed on this Homework task. I already spent around 3-4 hours. 
+
